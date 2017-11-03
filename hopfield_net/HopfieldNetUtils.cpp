ea9747d3f7c524e0b6759a71edbe60d5856da21f
Alessandro Petrini (Unimi) <offramp.uno@gmail.com>
2017-11-03 13:59:40 +0100

    Rimosse funzioni duplicate in HopfilednetUtils

--- hopfield_net/HopfieldNetUtils.cpp
+++ hopfield_net/HopfieldNetUtils.cpp
@@ -1,19 +1,3 @@
-#include <iostream>
-#include <fstream>
 #include "HopfieldNetUtils.h"
 
-void saveGeneNames( std::map<int, std::string> * mappaGeniInv, int n, std::string filename ) {
-	std::ofstream fout( filename.c_str() );
-
-	if (fout.is_open()) {
-		std::cout << "Salvo i nomi su un file" << std::endl;
-
-		fout << n << std::endl;
-
-		for (int i = 0; i < n; i++) {
-			fout << mappaGeniInv->at(i) << std::endl;
-		}
-
-		fout.close();
-	}
-}
+// Un cazzo
