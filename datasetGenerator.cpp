aa6e0bbcb6981530663d773032942847364cfc43
Alessandro Petrini (Unimi) <offramp.uno@gmail.com>
2017-11-16 17:42:40 +0100

    dataset generator apposto

--- datasetGenerator.cpp
+++ datasetGenerator.cpp
@@ -3,18 +3,26 @@
 #include <cinttypes>
 #include <memory>
 #include <algorithm>
+#include <random>
 #include <string>
-#include "graph/graph.h"
+#include <vector>
+#include "graph/graph.h"	// in realta' non serve!
 
 std::vector<std::string> generateRandomName( const int n );
 
 int main( int argc, char ** argv ) {
 
-	uint32_t		nNodes /* = inserire */;
-	uint32_t		nClasses /* = inserire */;
+	uint32_t		nNodes = 500 /* = inserire */;
+	uint32_t		nClasses = 10 /* = inserire */;
 	std::string labelFileName( "generatedLabels.txt" );
 	std::string netFileName( "generatedNet.txt" );
 	// OPZIONALE: leggere i parametri precedenti da riga di comando
+
+	float probLabels = 0.05f;
+	float probDensity = 0.1f;
+	std::default_random_engine eng{ 5 };
+	std::uniform_real_distribution<> randR(0.0, 1.0);
+	std::normal_distribution<> randNorm(0, 0.1);
 
 	std::ofstream labelFile( labelFileName.c_str(), std::ios::out );
 	std::ofstream netFile( netFileName.c_str(), std::ios::out );
@@ -45,12 +53,13 @@ int main( int argc, char ** argv ) {
 	// esempio: "GO::001", poi "GO::002", ecc...
 	// Nel file devono essere salvati solo i nomi dei nodi positivi
 	for (uint32_t i = 0; i < nClasses; i++) {
-		//std::string currentClassName; 	<- completa
+		std::string currentClassName( classBaseName + std::to_string( i ) );
 		for (uint32_t j = 0; j < nNodes; j++ ) {
 			// estrazione di un numero random
-			// se estrazione ha successo (maggiore di una soglia, o vedi tu)
-			labelFile << /* currentClassName << */ " " << nodeNames[j] << std::endl;
-
+			// se estrazione ha successo
+			if (randR(eng) < probLabels) {
+				labelFile << nodeNames[j] << "\t" << currentClassName << std::endl;
+			}
 		}
 	}
 
@@ -61,6 +70,42 @@ int main( int argc, char ** argv ) {
 	// gia' implementata.
 	// Per la scrittura dell'arco generato, usare l'istruzione:
 	// netfile << nodeNames[i] << " " << nodeNames[j] << " " << randomWeight << std::endl;
+	uint64_t 	*	cumulDegs = new uint64_t[nNodes + 1];
+	uint64_t	*	neighs;
+	float		*	weights;
+	uint64_t		nEdges = 0;
+
+	std::fill(cumulDegs, cumulDegs + nNodes + 1, 0);
+
+	std::vector<uint64_t>	* edges = new std::vector<uint64_t>[nNodes];
+	for (uint32_t i = 0; i < nNodes - 1; i++) {
+		for (uint32_t j = i + 1; j < nNodes; j++)
+			if (randR(eng) < probDensity) {
+				edges[i].push_back(j);
+				edges[j].push_back(i);
+				cumulDegs[i + 1]++;
+				cumulDegs[j + 1]++;
+				nEdges += 2;
+			}
+	}
+	cumulDegs[0] = 0;
+	for (uint32_t i = 0; i < nNodes; i++)
+		cumulDegs[i + 1] += cumulDegs[i];
+
+	neighs = new uint64_t[nEdges];
+	for (uint32_t i = 0; i < nNodes; i++)
+		memcpy((neighs + cumulDegs[i]), edges[i].data(), sizeof(uint64_t) * edges[i].size());
+
+	for (uint32_t i = 0; i < nNodes; i++) {
+		for (uint64_t j = cumulDegs[i]; j < cumulDegs[i+1]; j++) {
+			netFile << nodeNames[i] << "\t" << nodeNames[neighs[j]] << "\t" << fabs( randNorm( eng ) ) << std::endl;
+			//netFile << i << "\t" << neighs[j] << "\t" <<  fabs( randNorm( eng ) ) << std::endl;
+		}
+	}
+
+
+
+
 
 
 	netFile.close();
@@ -79,8 +124,6 @@ std::vector<std::string> generateRandomName( const int n ) {
 	char stringa[slen + 1];
 	stringa[slen] = 0;
 
-	// Shhht! Zippete!
-	// https://www.youtube.com/watch?v=fK8mneO8yvU
 	for (int i = 0; i < n; i++) {
 		std::for_each( stringa, stringa + slen, [alphanum](char &c){c = alphanum[rand() % (sizeof(alphanum) - 1)];} );
 		out.push_back( std::string( stringa ) );
